1da599ac22a9b33f4df8af508d331e8f170582f0
Adasi <adasi@me.com>
2023-08-29 16:07:29 -0400

    Tweak EXP and Meso drop server settings.
    Reduce super nimblefeet speed advantage to be reasonable.

--- src/server/MapleStatEffect.java
+++ src/server/MapleStatEffect.java
@@ -446,7 +446,7 @@ public class MapleStatEffect {
                     addBuffStatPairToListIfNotZero(statups, MapleBuffStat.MAP_CHAIR, 1);
                 } else if ((sourceid == Beginner.NIMBLE_FEET || sourceid == Noblesse.NIMBLE_FEET || sourceid == Evan.NIMBLE_FEET || sourceid == Legend.AGILE_BODY) && ServerConstants.USE_ULTRA_NIMBLE_FEET == true) {
                     ret.jump = (short) (ret.speed * 4);
-                    ret.speed *= 15;
+                    ret.speed *= 5;
                 }
             }
 
