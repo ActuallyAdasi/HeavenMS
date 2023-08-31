b7bb146841abe1aa8046cbf26e51a8cf1157e6d8
Adasi <adasi@me.com>
2023-08-30 21:14:36 -0400

    remove dependency on yaml dependency with CVE - replace with jackson

--- src/config/YamlConfig.java
+++ src/config/YamlConfig.java
@@ -1,30 +1,28 @@
 package config;
 
-import com.esotericsoftware.yamlbeans.YamlReader;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 
+import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
 import java.util.List;
 
 
 public class YamlConfig {
-
     public static final YamlConfig config = fromFile("config.yaml");
     
     public List<WorldConfig> worlds;
     public ServerConfig server;
 
-    public static YamlConfig fromFile(String filename) {
+    public static YamlConfig fromFile(final String filename) {
         try {
-            YamlReader reader = new YamlReader(new FileReader(filename));
-            YamlConfig config = reader.read(YamlConfig.class);
-            reader.close();
-            return config;
-        } catch (FileNotFoundException e) {
+            final ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
+            return mapper.readValue(new File(filename), YamlConfig.class);
+        } catch (final FileNotFoundException e) {
             String message = "Could not read config file " + filename + ": " + e.getMessage();
             throw new RuntimeException(message);
-        } catch (IOException e) {
+        } catch (final IOException e) {
             String message = "Could not successfully parse config file " + filename + ": " + e.getMessage();
             throw new RuntimeException(message);
         }
